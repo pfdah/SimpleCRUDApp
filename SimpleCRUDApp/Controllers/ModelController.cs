a3e7bcb225e42cf59840f4a3d2aebbb4082f8bf0
prati <prati@DESKTOP-CNQP7AM>
2022-08-13 16:02:07 +0545

    Add project files.

---
+++ SimpleCRUDApp/Controllers/ModelController.cs
@@ -0,0 +1,24 @@
+﻿using SimpleCRUDApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleCRUDApp.Controllers
+{
+    public class ModelController : Controller
+    {
+        readonly TestEnt db = new TestEnt();
+        // GET: Model
+        public ActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
