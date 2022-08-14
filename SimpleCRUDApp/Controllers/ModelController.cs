c39be05d3480f9648502e3c2c3e8a1b62c2e4f4b
Pratik Dahal <pratikdahal700@gmail.com>
2022-08-14 21:14:37 +0545

    Finished Basic CRUD Operation Using ADO.NET Data Model

--- SimpleCRUDApp/Controllers/ModelController.cs
+++ SimpleCRUDApp/Controllers/ModelController.cs
@@ -27,5 +27,38 @@ namespace SimpleCRUDApp.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Edit(int id)
+        {
+            Item item = db.Items.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            Item item1 = db.Items.Find(item.ID);
+            item1.Name = item.Name;
+            item1.Description = item.Description;
+            item1.Price = item.Price;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            Item item = db.Items.Find(id);
+            return View(item);
+        }
+        public ActionResult Delete(int id)
+        {
+            Item item = db.Items.Find(id);
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Delete(Item item)
+        {
+            Item i1 = db.Items.Find(item.ID);
+            db.Items.Remove(i1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
